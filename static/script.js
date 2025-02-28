409236f0e01bc1f7a257821f2390d9522b1b24d6
Lovro <lovroperaic@gmail.com>
2025-02-28 17:39:59 +0100

    Add range input functionality and range input track styling

--- static/script.js
+++ static/script.js
@@ -1,3 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const FORM = document.querySelector('form');
+    const CHAR_LENGTH_RANGE = document.querySelector('#char-length');
+    const CHAR_LENGTH_OUTPUT = document.querySelector('#char-length-output');
+
+    handleRangeChange(CHAR_LENGTH_RANGE, CHAR_LENGTH_OUTPUT);
+    
+    FORM.addEventListener('submit', handleSubmit);
+    CHAR_LENGTH_RANGE.addEventListener('input', () => handleRangeChange(CHAR_LENGTH_RANGE, CHAR_LENGTH_OUTPUT));
+
+
+    function handleRangeChange(range, output) {
+        styleRange(range);
+        output.value = range.value;
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
+    function percentage(value, total) {
+        return value / total * 100;
+    }
+
+    function styleRange(range) {
+        let valuePercent = percentage(range.value, range.max);
+        
+        if (range.value === range.max) {
+            range.classList.remove('empty')
+            range.classList.add('full')
+        } else if (range.value === range.min) {
+            range.classList.remove('full')
+            range.classList.add('empty')
+        } else {
+           range.classList.remove('full', 'empty')
+        }
+
+        range.style.background = `
+            linear-gradient(
+                90deg,
+                var(--range-progress) ${valuePercent}%,
+                var(--range-background) ${valuePercent}%
+            )
+        `;
+    }
 });
